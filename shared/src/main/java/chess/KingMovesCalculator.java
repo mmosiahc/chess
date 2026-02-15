cfce7be84e1f0eb6ad0c318c43d60afdd395fbf1
Michael Crow <mmosiahc@gmail.com>
2026-02-14 21:23:51 -0700

    Changed two variable names to improve naming.

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -6,11 +6,11 @@ import java.util.Collection;
 public class KingMovesCalculator extends BaseMovesCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> calculateMoves (ChessBoard board, ChessPosition position, ChessPiece piece) {
-         final int[][] KING_MOVES = {
+         final int[][] kingMoves = {
                 {1,0}, {-1,0}, {0,1}, {0,-1}, {1,1}, {1,-1}, {-1,1}, {-1,-1}
         };
         Collection<ChessMove> moves = new ArrayList<>();
-        applyKingAndKnightMoves(board, position, KING_MOVES, moves);
+        applyKingAndKnightMoves(board, position, kingMoves, moves);
         return moves;
     }
 
--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -6,11 +6,11 @@ import java.util.Collection;
 public class KnightMovesCalculator extends BaseMovesCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> calculateMoves (ChessBoard board, ChessPosition position, ChessPiece piece) {
-        final int[][] KNIGHT_MOVES = {
+        final int[][] knightMoves = {
                 {2,1}, {2,-1}, {1,2}, {1,-2}, {-2,1}, {-2,-1}, {-1,2}, {-1,-2}
         };
         Collection<ChessMove> moves = new ArrayList<>();
-        applyKingAndKnightMoves(board, position, KNIGHT_MOVES, moves);
+        applyKingAndKnightMoves(board, position, knightMoves, moves);
         return moves;
     }
 }
