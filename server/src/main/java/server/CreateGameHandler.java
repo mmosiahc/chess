1b9ad100e40d0750ee347bc285fabf25c8408cb6
Michael Crow <mmosiahc@gmail.com>
2026-03-17 23:16:45 -0600

    impl(ph3) CreateGames endpoint 70%. test Next: Add handler to server and test.

--- server/src/main/java/server/CreateGameHandler.java
+++ server/src/main/java/server/CreateGameHandler.java
@@ -21,5 +21,6 @@ public class CreateGameHandler extends BaseHandler{
         CreateGameRequest request = new CreateGameRequest(token, name);
         CreateGameResult result = service.createGame(request);
         ctx.json(new Gson().toJson(result));
+
     }
 }
