b6ffd21e3b4095e486520b9d9becde6f015841a9
Michael Crow <mmosiahc@gmail.com>
2026-03-17 22:01:01 -0600

    impl(ph3) CreateGames endpoint 70%. Created CreateGameHandler Next: Add handler to server and test.

---
+++ server/src/main/java/server/CreateGameHandler.java
@@ -0,0 +1,25 @@
+package server;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import io.javalin.http.Context;
+import service.CreateGameRequest;
+import service.CreateGameResult;
+import service.GameService;
+
+public class CreateGameHandler extends BaseHandler{
+    final GameService service;
+
+    public CreateGameHandler(GameService service) {
+        this.service = service;
+    }
+
+    void createGame(Context ctx) throws DataAccessException, RuntimeException {
+        String token = getAuthHeaderObject(ctx, String.class);
+        String name = getBodyObject(ctx, String.class);
+        System.out.println(name);
+        CreateGameRequest request = new CreateGameRequest(token, name);
+        CreateGameResult result = service.createGame(request);
+        ctx.json(new Gson().toJson(result));
+    }
+}
