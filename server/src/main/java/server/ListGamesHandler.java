9989fb69353807591d41946558cac4dc08f4ca7c
Michael Crow <mmosiahc@gmail.com>
2026-03-17 18:28:39 -0600

    impl(ph3) ListGames endpoint 70%. Created ListGamesHandler.java Next: Add ListGamesHandler to server and test.

---
+++ server/src/main/java/server/ListGamesHandler.java
@@ -0,0 +1,25 @@
+package server;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import io.javalin.http.Context;
+import service.GameService;
+import service.ListGamesRequest;
+import service.ListGamesResult;
+
+import java.util.Collection;
+
+public class ListGamesHandler extends BaseHandler{
+    final GameService service;
+
+    public ListGamesHandler(GameService service) {
+        this.service = service;
+    }
+
+    void listGames(Context ctx) throws DataAccessException, RuntimeException {
+        String token = getAuthHeaderObject(ctx, String.class);
+        ListGamesRequest request = new ListGamesRequest(token);
+        Collection<ListGamesResult> results = service.listGames(request);
+        ctx.json(new Gson().toJson(results));
+    }
+}
