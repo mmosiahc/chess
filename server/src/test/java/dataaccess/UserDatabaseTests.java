99b9bc87d0237d2fa3b4c798d24ac6f51aafdc99
Michael Crow <mmosiahc@gmail.com>
2026-03-30 19:19:52 -0600

    impl(ph4) Troubleshooting AutoGrader

--- server/src/test/java/dataaccess/UserDatabaseTests.java
+++ server/src/test/java/dataaccess/UserDatabaseTests.java
@@ -1,64 +1,42 @@
 package dataaccess;
 
 import model.UserData;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.sql.SQLException;
-import java.sql.Statement;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserDatabaseTests extends BaseDatabaseTest {
+    private final UserDatabase users = new UserDatabase();
+
+    @AfterEach
+    void tearDown() throws DataAccessException {
+        users.clear();
+    }
 
     @Test
     @DisplayName("Get User - Successful")
     void getExistingUser() throws DataAccessException {
-        UserData user;
-        String insertUserStatement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
-        try (var conn = DatabaseManager.getConnection()) {
-            try (var preparedStatement = conn.prepareStatement(insertUserStatement, Statement.RETURN_GENERATED_KEYS)) {
-                preparedStatement.setString(1, "Mike");
-                preparedStatement.setString(2, "password");
-                preparedStatement.setString(3, "mike@chess.com");
-
-                preparedStatement.executeUpdate();
-
-                UserDatabase users = new UserDatabase();
-                user = users.getUser( "Mike");
-            }
-        }catch (SQLException e) {
-            throw new DataAccessException("failed to connect");
-        }
+        UserData user = new UserData("Mike", "password", "mike@chess.com");
+        users.createUser(user);
+        user = users.getUser("Mike");
         assertEquals("Mike", user.username());
+        assertEquals("mike@chess.com", user.email());
     }
 
     @Test
     @DisplayName("Get User - username doesn't exist")
     void getNonExistentUser() throws DataAccessException {
-        UserData user;
-        String insertUserStatement = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
-        try (var conn = DatabaseManager.getConnection()) {
-            try (var preparedStatement = conn.prepareStatement(insertUserStatement, Statement.RETURN_GENERATED_KEYS)) {
-                preparedStatement.setString(1, "Mike");
-                preparedStatement.setString(2, "password");
-                preparedStatement.setString(3, "mike@chess.com");
-
-                preparedStatement.executeUpdate();
-
-                UserDatabase users = new UserDatabase();
-                user = users.getUser("username");
-            }
-        }catch (SQLException e) {
-            throw new DataAccessException("failed to connect");
-        }
+        UserData user = new UserData("newMike", "password", "mike@chess.com");
+        users.createUser(user);
+        user = users.getUser("unknownUser");
         assertNull(user);
     }
 
     @Test
     @DisplayName("Create User - Successful")
     void createNewUser() throws DataAccessException {
-        UserDatabase users = new UserDatabase();
         UserData user = new UserData("newUser", "password", "newUser@chess.com");
         users.createUser(user);
         UserData retrievedUser = users.getUser("newUser");
@@ -67,10 +45,11 @@ public class UserDatabaseTests extends BaseDatabaseTest {
 
     @Test
     @DisplayName("Create User - Username taken")
-    void createDuplicateUser() {
+    void createDuplicateUser() throws DataAccessException{
         UserDatabase users = new UserDatabase();
         UserData user = new UserData("newUser", "password", "newUser@chess.com");
-        assertThrows(DataAccessException.class, () -> users.createUser(user));
+        users.createUser(user);
+        assertThrows(AlreadyTakenException.class, () -> users.createUser(user));
     }
 
     @Test
