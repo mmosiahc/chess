f23aadf8a84eead0e5b785129386d3cb74a74b41
Michael Crow <mmosiahc@gmail.com>
2026-03-31 18:00:45 -0600

    impl(ph4) Troubleshooting AutoGrader

--- server/src/test/java/passoff/server/StandardAPITests.java
+++ server/src/test/java/passoff/server/StandardAPITests.java
@@ -30,7 +30,12 @@ public class StandardAPITests {
 
     @BeforeAll
     public static void init() throws DataAccessException {
-        server = new Server();
+        try{
+            server = new Server();
+        }catch (Exception e) {
+            throw new DataAccessException("Couldn't construct server", e);
+        }
+
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
 
